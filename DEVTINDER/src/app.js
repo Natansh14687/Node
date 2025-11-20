f3bf4ea43779c7bb292e136076f56e5bc870f928
Natansh14687 <natansh522@gmail.com>
2025-11-20 19:35:13 +0530

    router query and params

--- DEVTINDER/src/app.js
+++ DEVTINDER/src/app.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const app = express();
 
-app.use("/test", (req,res)=>{
-    res.send("This is the /test route for server 7777");
-});
+app.get("/user",(req, res)=>{
+    console.log(req.query);
+    res.send({firstName:"Natansh", lastName:"Khurana"});
+    
+})
 
-app.use("/hello", (req, res)=>{
-    res.send("This is the /hello route of server 7777");
-});
-
-app.use((req,res)=>{
-    res.send("The server 7777 is started");
-});
+app.get("/hello/:userid/:name/:password",(req, res)=>{
+    console.log(req.params);
+    res.send("Server is running");
+})
 
 
 app.listen(7777, ()=>{
