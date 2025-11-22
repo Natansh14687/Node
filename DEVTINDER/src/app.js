ee8c7537af3b8ebb7c6d4457a732fa20b309e4c5
Natansh14687 <natansh522@gmail.com>
2025-11-22 11:44:38 +0530

    played with model apis

--- DEVTINDER/src/app.js
+++ DEVTINDER/src/app.js
@@ -18,6 +18,110 @@ app.post("/signup", async (req, res)=>{
     }
 })
 
+app.get("/user", async (req, res)=>{
+    const lastName = req.body.lastName;
+    console.log(lastName);
+    
+    try{
+        const users = await User.find({lastName : lastName});
+        console.log(users);
+        res.send(users);
+    }catch(err){
+        res.send("unable to connect to the database");
+    }
+})
+
+app.get("/feed", async (req, res)=>{
+    
+    try{
+        const users = await User.find({});
+        console.log(users);
+        res.send(users);
+    }catch(err){
+        res.send("unable to connect to the database");
+    }
+})
+
+app.get("/search", async (req, res)=>{
+    const id = req.body.id;
+    console.log(id);;
+    
+    try{
+        const users = await User.findById(id);
+        console.log(users);
+        res.send(users);
+    }catch(err){
+        res.send("unable to connect to the database");
+    }
+})
+
+app.get("/searchbyage", async (req, res)=>{
+    const age = req.body.age;
+    console.log(age);
+    
+    try{
+        const users = await User.findOne({age : age});
+        console.log(users);
+        res.send(users);
+    }catch(err){
+        res.send("unable to connect to the database");
+    }
+});
+
+app.delete("/user", async (req, res) => {
+    const id = req.body.id;
+    console.log(id);
+    
+    try{
+        const user = await User.findByIdAndDelete(id);
+        res.send("user deleted successfully");
+    }catch(err){
+        console.log("user can't be deleted");
+        
+    }
+});
+
+app.delete("/userbyfirstname", async (req, res)=>{
+    const firstName = req.body.firstName;
+    console.log(firstName);
+    
+    try{
+        const user = await User.deleteOne({firstName : firstName});
+        res.send("User with provided firstName deleted successfully");
+    }catch(err){
+        res.status(400).send("user can't be deleted");
+    }
+})
+
+app.patch("/user", async (req, res) => {
+    const data = req.body;
+    const id = req.body.id;
+    console.log(data);
+    
+
+    try{
+        const user = await User.findByIdAndUpdate(id, {firstName : data.firstName});
+        res.send("user " + user + " updated successfully");
+    }catch(err){
+        console.error("User can't be updated");
+    }
+});
+
+app.patch("/userbydesc", async (req, res) => {
+    const data = req.body;
+    console.log(data);
+    
+
+    try{
+        const user = await User.findOneAndUpdate({description : data.description}, {firstName : data.firstName}, {returnDocument : 'after'});
+        res.send("user " + user + " updated successfully");
+    }catch(err){
+        console.error("User can't be updated");
+    }
+});
+
+
+
 
 connectDB()
 .then(()=>{
