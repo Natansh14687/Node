3399d63932f4d61f34a8fc0286eeab1e3d9c8459
Natansh14687 <natansh522@gmail.com>
2025-11-22 10:14:33 +0530

    database-init with static data

--- DEVTINDER/src/app.js
+++ DEVTINDER/src/app.js
@@ -2,6 +2,27 @@ const express = require("express");
 const app = express();
 
 const connectDB = require("./config/database");
+const User = require("./models/user");
+
+app.post("/signup", async (req, res)=>{
+    const userData = {
+        firstName : "Natansh",
+        lastName : "Khurana",
+        age : 22,
+        gender : "male",
+        description : "A boy with small goals",
+        skills : ["drawing", "singing"]
+    }
+
+    const user = new User(userData);
+    try{
+        await user.save();
+        res.send("user data sent to the database successfully");
+    }catch(err){
+        res.send("unable to connect to the database");
+    }
+})
+
 
 connectDB()
 .then(()=>{
