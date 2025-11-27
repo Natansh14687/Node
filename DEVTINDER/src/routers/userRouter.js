dc62fbc348c40ede4b68d4e764509b8804dcfac2
Natansh14687 <natansh522@gmail.com>
2025-11-27 11:35:10 +0530

    built feed api

--- DEVTINDER/src/routers/userRouter.js
+++ DEVTINDER/src/routers/userRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const connectionRequests = require("../models/connectionRequests");
+const User = require("../models/user");
 const userRouter = express.Router();
 
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
@@ -53,5 +54,44 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     res.status(400).send(err.message);
   }
 });
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try{
+    const loggedInUserId = req.user._id;
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const max = limit<50 ? limit : 50;
+    const skip = (page-1)*max;
+    
+
+
+
+    const userConnections = await connectionRequests.find({
+      $or : [
+        {toUserId : loggedInUserId},
+        {fromUserId : loggedInUserId}
+      ]
+    }).select("fromUserId toUserId");
+
+
+    const hideUsersFromFeed = new Set();
+
+    userConnections.forEach(connection => {
+      hideUsersFromFeed.add(connection.fromUserId.toString());
+      hideUsersFromFeed.add(connection.toUserId.toString());
+    });
+
+    const userFeed = await User.find({
+      _id : {$nin : Array.from(hideUsersFromFeed)}
+    }).select("firstName lastName").skip(skip).limit(max);
+
+    res.send(userFeed);
+
+
+
+  }catch(err){
+    res.status(400).send(err.message);
+  }
+})
 
 module.exports = userRouter;
